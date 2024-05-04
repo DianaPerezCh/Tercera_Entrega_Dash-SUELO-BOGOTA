2f5886e781d54a343e4c6d5793bc4c2012bd6195
DianaPerezCh <dianaperezch23@gmail.com>
2024-05-04 00:16:25 -0500

    actualizacion

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 # Se importa el franted
 from frontend.Presentacion import presentacion, Finalizacion
 from backend.respuesta_sismica import Tituto1, RespuestaSismica, Division, Poblacion_RespuestaSismica
-from backend.respuesta_sismica import DensidadPoblacional, consultarLocalidad
+from backend.respuesta_sismica import DensidadPoblacional, consultarLocalidad, consultarZonaSismica
 from frontend.Zonificacion_Geotecnica import zonificacionGeotecnica
 from frontend.Geologia_Urbana import GeologiaUrbana
 from frontend.Geologia_Rural import Geologiarural
@@ -27,7 +27,10 @@ app.layout = dbc.Container([
             dbc.Col(Division, md=1,style={'background-color':'white','textAlign':'center',"color":"white"}, ),
             dbc.Col(Poblacion_RespuestaSismica, md=4,style={'background-color':'#E67E22','textAlign':'center',"color":"black"}, ),
         ]),
-        html.Hr()
+        html.Hr(),
+    zonificacionGeotecnica,
+    GeologiaUrbana,
+    Geologiarural
     ]
     )
 
@@ -38,6 +41,7 @@ app.layout = dbc.Container([
 
 def update_map(localidad_consultada):
     return consultarLocalidad(localidad_consultada)
+
 
 
 
--- backend/respuesta_sismica.py
+++ backend/respuesta_sismica.py
@@ -7,6 +7,7 @@ from PIL import Image
 import geopandas as gpd
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 
 #token = open(".mapbox_token").read()
 
@@ -17,48 +18,82 @@ Tituto1 = html.H3('Zonas de Respuesta Sismica')
 
 
 Localidades = gpd.read_file('data\Localidades')
+#Localidades = Localidades.to_crs("WGS84")
+#Localidades_index=Localidades.set_index("RESSIS")
 ZonaSismica = gpd.read_file('data\Respuesta Sísmica')
+#ZonaSismica = ZonaSismica.to_crs("WGS84")
 DensidadPoblacional=gpd.read_file('data\Centroides_Manzanas.geojson')
 
 def consultarLocalidad(localidad_consultada):
 
-    localidad_buscada = Localidades.query(f"LocNombre == '{localidad_consultada.upper()}'")
+    localidad_buscada = Localidades.query(f"LocNombre == '{localidad_consultada}'")
+    #ZonaSismica_RESSIS=ZonaSismica.set_index("RESSIS")
 
-    localidad_buscada_4326 = localidad_buscada.to_crs(epsg=4326)
-    #ZonaSismica_4326 = ZonaSismica.to_crs(epsg=4326)
+    #localidad_buscada_4326 = localidad_buscada.to_crs(epsg=4326)
 
-    #ZonaSismica_Localidad = gpd.overlay(
-    #localidad_buscada_4326, ZonaSismica_4326, how='intersection')
+    ZonaSismica_4686 = ZonaSismica.to_crs(epsg=4686)
+
+    ZonaSismica_Localidad = gpd.overlay(
+    localidad_buscada, ZonaSismica_4686, how='intersection')
+    ZonaSismica_Localidad=ZonaSismica_Localidad.set_index("RESSIS")
+
+
+    
+   # ZonaSismica_Localidad['randNumCol']= np.random.randint(0, 10, ZonaSismica_Localidad.shape[0]).astype('str')
+
+       
+    # Genera mapa de localidades
+
+    fig = px.choropleth_mapbox(
+        geojson=ZonaSismica_Localidad.geometry,
+        locations=ZonaSismica_Localidad.index,
+        #legend=True,
+        #legend_kwds={"loc":"upper left"}
+    )
+
+
+
+
+        #data_frame = ZonaSismica_Localidad.set_index("RESSIS"), # Usar el ID como índice de los datos
+        #geojson = ZonaSismica_Localidad.geometry,                # La geometría
+        #locations = ZonaSismica_Localidad.index,                 # El índice de los datos
+        #color = 'unemp_rate',
+        #mapbox_style = 'open-street-map',
+        #center = dict(lat = 4.33, lon = -74.08),
+        #zoom = 4)
+
+    
+        #geojson=ZonaSismica_Localidad.geometry,
+        #locations=ZonaSismica_Localidad.index,
+
+        #data_frame = ZonaSismica_Localidad.set_index("RESSIS")
     
 
+    #fig.add_trace(
+     #   go.Scattermapbox(
+      #      lat=localidad_buscada_4326.y,
+       #     lon=localidad_buscada_4326.x,
+        #    mode='markers',
+         #   marker=go.scattermapbox.Marker(
+          #      size=9,
+           #     color='red'
+            #),
+            #text=localidad_buscada_4326['LocNombre'],
+            #hoverinfo='text'
+   #     )
 
-
-
-    # Genera mapa de localidades
-    fig = px.choropleth_mapbox(
-        geojson=localidad_buscada_4326.geometry,
-        locations=localidad_buscada_4326.Index
-    )
-
-    fig.add_trace(
-        go.Scattermapbox(
-            lat=localidad_buscada_4326.y,
-            lon=localidad_buscada_4326.x,
-            mode='markers',
-            marker=go.scattermapbox.Marker(
-                size=9,
-                color='red'
-            ),
-            text=localidad_buscada_4326['LocNombre'],
-            hoverinfo='text'
-        )
-
-    )
+    #)
 
     fig.update_layout(
+        #ZonaSismica_Localidad=ZonaSismica_Localidad.set_index("RESSIS"),
+        #color = 'unemp_rate',
         mapbox_style="open-street-map",
-        mapbox_zoom=5,
-        mapbox_center = {"lat": 4.6, "lon": -74},
+        #showlegend=False,
+        #margin={"r":0,"t":0,"l":0,"b":0},
+        #color="randNumCol",
+        mapbox_zoom=8,
+        mapbox_center = {"lat": 4.33, "lon": -74.08},
+    
     )
 
     #ZonaSismica_Localidad_grafica=px.ZonaSismica_Localidad.election()
@@ -81,19 +116,15 @@ def consultarLocalidad(localidad_consultada):
 
         #return 'El total de habitantes de la localidad de' + str(localidad_buscada) +'ubicados en una zonificación del suelo de respuesta sismica' + str(ZonaSismica_consultada) + 'es de' + str(Total_habitantes)
    
-    return fig 
+    return fig
 
 
 
 #Se crea espacio para la columna en blanca divisoria.
 Division = ''
 
-#Se crea espacio para la elección de la respuesta sismica del suelo titulo, menu y datos.
-Suelos = html.Div([
-        html.H3('Zonificación del suelo'),
-        html.Hr()
-    ],
-    )
+
+
 
 RespuestaSismica = dbc.Container(
     [
@@ -107,7 +138,7 @@ RespuestaSismica = dbc.Container(
                 id="mapa",
                 style={
                     'width': '100%', 
-                    "height": "600px"
+                    "height": "600px",'background-color':'#FFCC80'
                     },
         )
        ])
@@ -115,18 +146,33 @@ RespuestaSismica = dbc.Container(
     fluid=True
 )
 
+ConsultaZonaSismica_Localidad=consultarLocalidad
+
+
+def consultarZonaSismica(ZonaSismica_consultada):
+    ZonaSismica_buscada = ConsultaZonaSismica_Localidad.query(f"RESSIS == '{ZonaSismica_consultada}'")
+    DensidadPoblacional_4686 = ZonaSismica.to_crs(epsg=4686)
+    ZonaSismica_Densidad = gpd.overlay(
+    ZonaSismica_buscada, DensidadPoblacional_4686, how='intersection')
+    Total_Habitantes=ZonaSismica_Densidad["PER_S010"].sum()
+
+
+
+    return 'El total de habitantes de la localidad de' +Total_Habitantes
+
+
 
 Poblacion_RespuestaSismica = dbc.Container(
     [
         html.H3('Zonificación del suelo'),
         html.Hr(),
        html.Div([
-           dcc.RadioItems(ZonaSismica['RESSIS'], id='ZonaSismica_consultada',
+           dcc.RadioItems(ZonaSismica['RESSIS'].unique(), id='ZonaSismica_consultada',
                         style={'background-color':'#FFCC80',"font-weight": "bold",'textAlign':'left'}),
             html.Hr(),
             html.H3('Cantidad de Habitantes'),
             html.Hr(),
-            html.H5(id='Total_habitantes')
+            html.H5(id ='PoblacionTotal')
     
        ]),
 
